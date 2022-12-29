4ae05e9ad84984664af70925fcf8c3d4995ad300
ognjen-nedic <ognjen.n94@gmail.com>
2022-12-29 15:25:13 +0100

    images fix

--- public/Components/Picker.js
+++ public/Components/Picker.js
@@ -3,14 +3,14 @@ import Image from 'next/image'
 
 const Picker = (props) => {
   return (
-    <div loading="lazy" className="sign-pick" style={{backgroundImage: [`url(../files/images/${props.planet}.jpg)`,`url(../files/images/nightsky.jpg)`]}}>
-        <div className="sign-blur-effect"><img src={`../files/images/${props.sign}.svg`} alt={`${props.sign} sign icon`}/></div>
+    <div loading="lazy" className="sign-pick" style={{backgroundImage: [`url(/astrology/files/images/${props.planet}.jpg)`,`url(/astrology/files/images/nightsky.jpg)`]}}>
+        <div className="sign-blur-effect"><img src={`/astrology/files/images/${props.sign}.svg`} alt={`${props.sign} sign icon`}/></div>
         {props.signArray.map((element,index) => {
           return (
             <div className="sign-element" key={index}>
               <label className="sign-box">
                 <input type="radio" name="sign" value={element} id={element} onChange={(e) => props.setSign(e.target.value)}/>
-                <img src={`../files/images/${element}.svg`} alt={`${element} sign icon`}/>
+                <img src={`/astrology/files/images/${element}.svg`} alt={`${element} sign icon`}/>
               </label>
             </div>)        
         })}
