492fef4adbcc1b8f1755c57d4f5a08efb1b8c1ae
ognjen-nedic <ognjen.n94@gmail.com>
2022-12-29 14:56:39 +0100

    desktop core

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/background.css'
+import '../styles/reset.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
