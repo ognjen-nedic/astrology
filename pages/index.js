4e06fedcf5d66d2131d3b55d28da6ebce7ef0b42
ognjen-nedic <ognjen.n94@gmail.com>
2023-01-03 12:07:37 +0100

    open graph

--- pages/index.js
+++ pages/index.js
@@ -75,8 +75,11 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Astrology Screensaver</title>
+        <title>Daily horoscope</title>
         <meta name="description" content="Screensaver for lobby with astrology theme" />
+        <meta property="og:title" content="Daily horoscope" />
+        <meta property="og:description" content="Screensaver for lobby with astrology theme" />
+        <meta property="og:image" content="/astrology/files/images/og-background.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
