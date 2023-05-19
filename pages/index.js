11b288e005f711a2717b611ad7242df9c8bdae98
ognjen-nedic <ognjen.n94@gmail.com>
2023-05-19 17:01:41 +0200

    descriptions

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,21 @@ import PreloadedImage from '../public/Components/PreloadedImage'
 
 
 export default function Home() {
-  const data = { aries: { description: "Aries description", date_range: "March 21 - April 19" }
-  , taurus : { description: "Taurus description", date_range: "April 20 - May 20" }
-  , gemini : { description: "Gemini description", date_range: "May 21 - June 21" }
-  , cancer : { description: "Cancer description", date_range: " June 22 - July 22" }
-  , leo : { description: "Leo description", date_range: "July 23 - August 22" }
-  , virgo : { description: "Virgo description", date_range: "August 23 - September 22" }
-  , libra : { description: "Libra description", date_range: "September 23 - October 23" }
-  , scorpio : { description: "Scorpio description", date_range: "October 24 - November 21" }
-  , sagittarius : { description: "Sagittarius description", date_range: "November 22 - December 21" }
-  , capricorn : { description: "Capricorn description", date_range: "December 22 - January 19" }
-  , aquarius : { description: "Aquarius description", date_range: "January 20 - February 18" }
-  , pisces : { description: "Pisces description", date_range: "February 19 - March 20" } };
+  
+  const data = 
+  { aries: { description: "You know how to express yourself precisely. An art of charming aesthetics, your inclination will be beautiful and patient toward your sweetheart.", date_range: "March 21 - April 19" }
+  , taurus : { description: "Having a great prospect for today, the Taurus horoscope favors new connections as well with consolidation of long lasting ones. Full of optimism and enthusiasm, your love life is set to be on a definite upward trajectory.", date_range: "April 20 - May 20" }
+  , gemini : { description: "Taken Gemini signs will enjoy a quiet day inside with their loved one. Venus is affecting single Gemini signs heavily today, especially at night. Flirting with an Taurus will boost your ego.", date_range: "May 21 - June 21" }
+  , cancer : { description: "Mercury is pulsating energy which is going to make communication with your other half a lot easier. Try being more affectionate towards your partner.", date_range: " June 22 - July 22" }
+  , leo : { description: "Taken signs will feel passionate and crave intimacy. Single signs are going to feel fabulous and glorious around other fire signs today. Flirting is going to be a must.", date_range: "July 23 - August 22" }
+  , virgo : { description: "Venus is sending energy that you can feel in your heart. Your love life is going amazing, and you find it easy to make deep and sensual connections with other people.", date_range: "August 23 - September 22" }
+  , libra : { description: "Single Libra signs, don't settle for less than what you deserve. (You deserve the best!) Married signs might have a small argument that is related to your different tastes in things.", date_range: "September 23 - October 23" }
+  , scorpio : { description: "Single signs may flirt with a Cancer sign today. Taken signs, you will feel some tension in your relationship, but it's nothing that a nice dinner date can't fix. Be a little imaginative and try to surprise them.", date_range: "October 24 - November 21" }
+  , sagittarius : { description: "Sometimes, relationships don't work out. Stop fooling yourself that they are the one. Have a healthy conversation with them about this and let the both of you go your own way.", date_range: "November 22 - December 21" }
+  , capricorn : { description: "It is likely that single signs will enjoy flirting with fire signs today. Taken signs are going to have a wonderful day with their partner, full of love and care.", date_range: "December 22 - January 19" }
+  , aquarius : { description: "Single Aquarius signs might feel very compatible with an air sign. They will be mysterious and open for any kind of conversation.", date_range: "January 20 - February 18" }
+  , pisces : { description: "Work with conflicting energy to build a better understanding of a loved one and their desires. Breakthrough a wall and move on to the next phase of your relationship.", date_range: "February 19 - March 20" } };
+
   const [loading, setLoading] = useState(true)
   const [sign, setSign] = useState('aries');
   const [planet, setPlanet] = useState('mars');
